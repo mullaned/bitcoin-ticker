507237df6a15a21f6585a185c930f8cdc8b62a36
David M <mullaned@gmail.com>
2018-12-03 15:12:11 +0000

    jQuery request

--- jq.js
+++ jq.js
@@ -1,16 +1,22 @@
 let priceDisplay = document.querySelector('#price');
 
-$.ajax({
-    method: 'GET',
-    url: 'https://api.coindesk.com/v1/bpi/currentprice.json'
-})
-.done((res) => {
-    console.log(res);
-    let Price = JSON.parse(res).bpi.EUR.rate;
-    console.log(Price);
-    Price = Price.split('.');
-    
-    priceDisplay.innerHTML = '&euro;'+Price[0] + '.' + Price[1][0] + Price[1][1];
+setInterval(() => {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    })
+    .done((res) => {
+        
+        // console.log(res);
+        let Price = JSON.parse(res).bpi.EUR.rate;
+        console.log(Price);
+        Price = Price.split('.');
+        
+        priceDisplay.innerHTML = '&euro;'+Price[0] + '.' + Price[1][0] + Price[1][1];
+       
+    })
+    .fail(() => {
+        console.log("There was a error");
+    })       
+}, 2000)
 
-    
-})
